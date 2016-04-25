74ef68aade5b8d5f22fa50fb1b1342e18d66b100
Kshitiz Sharma <kshitizsharma38@gmail.com>
2016-04-25 18:18:26 +0530

    Option Menu and sync

--- src/UDP.java
+++ src/UDP.java
@@ -698,6 +698,7 @@ public class UDP implements Runnable, WindowListener, ActionListener {
         buttonClose.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                leaveRoomMsg();
                 frameMain.setVisible(false);
                 lobbyServer.closeSocket();
                 socket.disconnect();
